8a9364a80e5ccc9ae0903f8b849c7b6a99eab5e6
valerianoGonzalez <valerianogonzalez070402@gmail.com>
2023-11-06 11:38:42 -0400

    se construyo parte de la logica  de la sec-1  para cuando el suusuario escoja el tipo y color

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+"extrict-mode"
 const loader =document.querySelector("#loader")
 const container = document.querySelector("#container")
 function loadPage(){
@@ -5,7 +6,7 @@ window.addEventListener("load",function(){
   container.style.display = "block"
   loader.style.display = "none"
 console.log("se cargo la pagina")
-
+//audio.play();
 
 
 })
@@ -19,7 +20,7 @@ const body = document.querySelector("body");
 const sound_btn = document.querySelector("#sound-button")
 
 
-
+// modo oscuro
 function lightPage(){
 let DarkMode = false; 
 
@@ -38,7 +39,12 @@ Light_btn.addEventListener("click", () => {
 });
 }
 lightPage();
+////
+
+
 const audio = new Audio("./img/Psychedelic Game.mp3")
+// musica de fondo boton
+
 function soundPage(){
  let active = false
     sound_btn.addEventListener("click",()=>{
@@ -56,3 +62,85 @@ function soundPage(){
     })
 }
 soundPage();
+
+
+
+///
+const formGame = document.querySelector("#form-sec1")
+const nickname = document.querySelector("#nickname")
+const gender = document.querySelector("#dropdown")
+const btn_play = document.querySelector("#play-btn1")
+const player = document.querySelector("#user-nick")
+const handsColor = document.querySelectorAll("#hand-colors")
+const user_img = document.querySelector("#user-color")
+
+function sendData(){
+  formGame.addEventListener("submit",function(event){
+event.preventDefault();
+//toma los datos del form y se los envia ala sec-2 donde esta el juego
+let name = nickname.value
+let type = gender.value 
+player.textContent = name
+
+//accedemos al color que el usuario a selecionado
+handsColor.forEach(hand_color => {
+    if(hand_color.checked){
+
+      var SelectColor = hand_color.value
+      console.log([name,type,SelectColor])
+      //aqui hacemos el cambio de color del jugador dependiendo lo que el usuario escoja
+      switch (type) {
+        case "men":
+            switch (SelectColor) {
+                case "white":
+                    user_img.src = "./img/user-white.svg";
+                    break;
+                case "indian":
+                    user_img.src = "./img/user-indian.svg";
+                    break;
+                case "black":
+                    user_img.src = "./img/user-black.svg";
+                    break;
+                default:
+                    // Acción por defecto si SelectColor no coincide con ningún caso
+                    break;
+            }
+            break;
+    
+        case "women":
+            switch (SelectColor) {
+                case "white":
+                    user_img.src = "./img/woman-user-white.svg";
+                    break;
+                case "indian":
+                    user_img.src = "./img/woman-user-indian.svg";
+                    break;
+                case "black":
+                    user_img.src = "./img/woman-user-black.svg";
+                    break;
+                default:
+                  
+                    break;
+            }
+            break;
+    
+        default:
+           
+            break;
+    }
+  
+}
+else{
+
+}
+   
+});
+
+  })
+
+
+
+}
+
+
+sendData();
